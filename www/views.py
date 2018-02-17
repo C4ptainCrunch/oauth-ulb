9ef304d1cdeced30da992159b75ae50c0035679e
Nikita Marchant <nikita.marchant@gmail.com>
2018-02-17 11:11:51 +0100

    Better homepage

--- www/views.py
+++ www/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import random
 
 
 def home(request):
-    return render(request, 'home.html')
+    img = random.choice(["gazon", "r42", 'resto', 'panneau'])
+    return render(request, 'home.html', {'image': img})
